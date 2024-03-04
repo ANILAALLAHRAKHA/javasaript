9b15609fa358404bc9e65b4f2abbdb645e6bcac0
ANILA <160001699+ANILAALLAHRAKHA@users.noreply.github.com>
2024-03-04 12:00:27 +0500

    word done

---
+++ app.js
@@ -0,0 +1,221 @@
+// chapter1
+// Q1
+// var A=+prompt("enter the value");
+// if(A >65){
+//     document.write("true")
+// }
+// else{
+// document.write("false")
+// }
+// var Z=+prompt("enter the value")
+// if(Z >90){
+//     document.write("true")
+// }
+// else{
+// document.write("false")
+// }
+// var a=+prompt("enter the value")
+// if(a >97){
+//     document.write("true")
+// }
+// else{
+// document.write("false")
+// }
+// var z=+prompt("enter the value")
+// if(z >122){
+//     document.write("true")
+// }
+// else{
+// document.write("false")
+// }
+// Q2
+// Function to accept two integers from the user
+// function compareIntegers() {
+//     // Accepting the first integer
+//     var num1 = prompt("Enter the first integer:");
+
+//     // Accepting the second integer
+//     var num2 = prompt("Enter the second integer:");
+
+//     // Check if the two integers are equal
+//     if (120 === 30) {
+//         console.log("The two integers are equal.");
+//     } else {
+//         // Display the larger integer
+//         var largerInteger = (200 > 120) ? num1 : num2;
+//         console.log("The larger integer is: " + largerInteger);
+//     }
+// }
+// Q3
+// var number =input(150);
+// if (number > 0){
+//     print("20");}
+// else (number < 0)
+//     print("-50")
+// Q4
+// var character=("vowel");
+// if(character){
+//     document.write("true")
+// }
+// else{
+//     document.write("fales")
+// }
+// Q5
+// // Step a: Store correct password in a JS variable
+// var correctPassword = "1234567890";
+
+// // Step b: Ask user to enter his/her password
+// var userPassword = prompt("Please enter your password:");
+
+// // Step c: Validate the two passwords
+// if (userPassword === "1234567890") {
+//     // i. Check if user has entered password
+//     alert("Please enter your password.");
+// } else if (userPassword === correctPassword) {
+//     // ii. Check if both passwords are same
+//     alert("Correct! The password you entered matches the original password.");
+// } else {
+//     alert("Incorrect password.");
+// }
+// Q6
+// var hour =+prompt("enter the time");
+// if (hour < 18) {
+// document.write("Good day")}
+// else{
+// document.write("Good evening");
+// }
+// // Q7
+// var hour=+prompt("enter the time");
+// price=+prompt("enter the price");
+// if(price =1900){
+//      document.write("true")
+// }
+// if(hour = 7){
+//     document.write("true")
+// }
+// else {
+// document.write("false")
+// }
+
+// chapter2
+// Q1
+// var city=("karachi")
+//    var city = prompt("Please enter the name of your city");
+//     if( city="karachi"){
+//         document.write("Welcome to the city of lights!")}
+//     else{
+//         document.write("Welcome to", city)}
+// Q2
+// var gender=+prompt("enter your gender");
+// if(gender =="male"){
+//     document.write("GOOD MORNING SIR!")
+// }
+// else{
+//     document.write("GOOD MORNING MA'AM!")
+// }
+// Q3
+
+// var color=prompt("enter the color")
+// if(color==red){
+//     document.write("")
+// }
+// Q4
+// var liter=prompt("enter fuel")
+// if(liter >0.25){
+//     document.write("“Please after some time refill the fuel”")
+// }
+// else{
+//       document.write("“Please refill the fuel in your car”")
+// }
+// Q5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// // }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// // Q6
+// var English = +prompt("95");
+//         Urdu= +prompt("55");
+//      Maths= +prompt("100");
+//     total=("English + Urdu + Maths % 250 *100")
+// var percentage=total/250*100
+// var percentage=+prompt("enter the value")
+
+//     if (percentage >= 90){
+//        document.write( "return 'A'")}
+//     else if (percentage >= 80){
+//        document.write( "return 'B'")}
+//     else if (percentage >= 70){
+//        document.write( "return 'C'")}
+//     else if (percentage >= 60){
+//        document.write( "return 'D'")}
+//     else if (percentage >= 40){
+//        document.write( "return 'E'")}
+//     else{
+//        document.write("return 'F'")}
+// // // Q7
+// var secret =(random(1, 10))
+// guess =("Guess the secret number (between 1 and 10): ")
+// if (guess == secret){
+//     document.write("Bingo! Correct answer.")}
+// else if( guess == secret + 1 || guess == secret - 1){
+//     document.write("Close enough to the correct answer.")}
+// else{
+//     document.write("Sorry, incorrect guess. The secret number was", secret_number)}
+// // Q8
+// var number=("3")
+//     if (number % 3 == 0){
+//         document.write(number +"is divisible by 3.")}
+//     else{
+//         document.write(number +"is not divisible by 3.")}
+// Q9
+var check_even_odd=prompt("enter the number");
+    if( number % 2 == 0){
+        document.write(number +"is an even number")}
+    else{
+        document.write(number +"is an odd number")}
+    
+       var num = ("Enter a number to check if it's even or odd: ");
+    
+
+
+
+
+
+
+
+
